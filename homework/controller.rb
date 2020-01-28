e9f05ba6dd3245a28e2858cebb6f6536a05535b0
augustas3000 <augusto.pasaulis@gmail.com>
2020-01-28 09:47:09 +0000

    logic corrected to escape non rps values

--- homework/controller.rb
+++ homework/controller.rb
@@ -7,9 +7,16 @@ get '/' do
   erb(:home)
 end
 
+# reg exp to limit this roiute to combinations of ock/paper/scissors?
 get '/:hand1/:hand2' do
+  # we bind the functionality of model Rsp, to the user's interaction with the views.
+  # @game is a global variable which will be displayed in a view called erb(:result)
   hand1 = params[:hand1]
   hand2 = params[:hand2]
   @game = Rps.compare(hand1, hand2)
   erb(:result)
+end
+
+get '/rules' do
+erb(:rules)
 end
--- homework/models/rock_paper_scissors.rb
+++ homework/models/rock_paper_scissors.rb
@@ -2,18 +2,29 @@
 class Rps
 
   def self.compare(arg1, arg2)
+
+
+
+
     outcomes = {
       'rock'     => { 'rock' => 'tie',   'paper' => 'lose',  'scissors' => 'win'},
       'paper'    => { 'rock' => 'win',   'paper' => 'tie',   'scissors' => 'lose'},
       'scissors' => { 'rock' => 'lose',  'paper' => 'win',   'scissors' => 'tie' }
     }
 
-    if outcomes[arg1][arg2] == 'win'
-      return "Player 1 wins by playing #{arg1.capitalize}!"
-    elsif outcomes[arg1][arg2] == 'tie'
-      return "It's a tie! Both players played #{arg1.capitalize}!"
+    # check condition if rock , scissors or paper.
+    if outcomes.has_key?(arg1) && outcomes.has_key?(arg2)
+
+      if outcomes[arg1][arg2] == 'win'
+        return "Player 1 wins by playing #{arg1.capitalize}!"
+      elsif outcomes[arg1][arg2] == 'tie'
+        return "It's a tie! Both players played #{arg1.capitalize}!"
+      else
+        return "Player 2 wins by playing #{arg2.capitalize}!"
+      end
+
     else
-      return "Player 2 wins by playing #{arg2.capitalize}!"
+      return "Wrong input! Please enter either rock, paper, or scissors."
     end
 
   end
